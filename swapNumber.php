57741f4b1686bba354c20b4eef521f7982d13eb9
suyog <suyogkadu00.sk>
2024-10-01 11:36:38 +0530

    swapTwoNumber

---
+++ swapNumber.php
@@ -0,0 +1,13 @@
+<?php
+
+$a=2;
+$b=3;
+
+$a=$a+$b; //$a=2+3; now $a=5;
+$b=$a-$b; //$b=5=3; now $b=2
+$a=$a-$b; //$a=5-3; now $a=3
+
+//so $a is now 3 and $b is now 2
+
+echo "a= ".$a."<br/> b= ".$b;
+?>
